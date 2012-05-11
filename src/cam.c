1f637a377eaa68b2be4ad73b57e513dcc3f97daa
Emily Eisenberg <xymostech@gmail.com>
2012-05-11 14:30:39 -0400

    Fixed camera so y axis is up

--- src/cam.c
+++ src/cam.c
@@ -4,7 +4,7 @@ void setup_cam(struct cam *my_cam) {
 	glLoadIdentity();
 	gluLookAt(my_cam->x, my_cam->y, my_cam->z,
 	          my_cam->x+cos(my_cam->xang)*sin(my_cam->yang),
-	          my_cam->y+sin(my_cam->xang)*sin(my_cam->yang),
-	          my_cam->z+cos(my_cam->yang),
+	          my_cam->y+cos(my_cam->yang),
+	          my_cam->z+sin(my_cam->xang)*sin(my_cam->yang),
 	          0, 1, 0);
 }
