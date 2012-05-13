4b814f3ee09a6898d3f7729b4ed928c199aac75d
Emily Eisenberg <xymostech@gmail.com>
2012-05-13 18:04:52 -0400

    Add basic drawing function for polygons

--- src/polygon.c
+++ src/polygon.c
@@ -14,6 +14,10 @@ void point_delete(struct point *point) {
 	free(point);
 }
 
+void point_draw(struct point *point) {
+	glVertex3f(point->x, point->y, point->z);
+}
+
 struct polygon* polygon_init() {
 	struct polygon* poly = malloc(sizeof(*poly));
 
@@ -29,4 +33,14 @@ void polygon_delete(struct polygon *poly) {
 void polygon_add_point(struct polygon *poly, struct point *point) {
 	point->next = poly->points;
 	poly->points = point;
+}
+
+void polygon_draw(struct polygon *poly) {
+	glBegin(GL_POLYGON);
+	struct point *point = poly->points;
+	while (point) {
+		point_draw(point);
+		point = point->next;
+	}
+	glEnd();
 }
