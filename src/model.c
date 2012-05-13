204e9df34d8fbbb324247c5781c88df0e735a55e
Emily Eisenberg <xymostech@gmail.com>
2012-05-13 18:30:16 -0400

    Add framework for model loading/drawing

--- src/model.c
+++ src/model.c
@@ -1,1 +1,13 @@
 #include "model.h"
+
+struct model* model_load(char *filename) {
+	
+}
+
+void model_delete(struct model *model) {
+	
+}
+
+void model_draw(struct model *model) {
+	
+}
--- src/model.h
+++ src/model.h
@@ -1,6 +1,17 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "polygon.h"
 
+struct model {
+	char *name;
+
+	struct polygon *polygons;
+};
+
+struct model* model_load(char*);
+void model_delete(struct model*);
+
+void model_draw(struct model*);
 
 #endif /* MODEL_H */
