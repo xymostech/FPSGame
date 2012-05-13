205e1387b3c49ae013252715f9971709f05f7582
Emily Eisenberg <xymostech@gmail.com>
2012-05-13 15:07:22 -0400

    Add mouselook function for mouse usage

--- src/mouse.c
+++ src/mouse.c
@@ -19,3 +19,9 @@ int mouse_window_y() {
 	glfwGetMousePos(&x, &y);
 	return *window_height() / 2 - y;
 }
+
+void mouse_get_mouselook_diff(int *x, int *y) {
+	*x = mouse_window_x();
+	*y = mouse_window_y();
+	glfwSetMousePos(*window_width()/2, *window_height()/2);
+}
--- src/mouse.h
+++ src/mouse.h
@@ -11,4 +11,6 @@ int mouse_right_pressed();
 int mouse_window_x();
 int mouse_window_y();
 
+void mouse_get_mouselook_diff(int *x, int *y);
+
 #endif /* MOUSE_H */
