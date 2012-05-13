3a63a1f022ca9afd1812b0871d0626f0cb547dc8
Emily Eisenberg <xymostech@gmail.com>
2012-05-13 13:02:44 -0400

    Added world framework for world objects/floors

--- src/world.c
+++ src/world.c
@@ -1,1 +1,17 @@
 #include "world.h"
+
+void world_object_delete(struct world_object *object) {
+	
+}
+
+struct world_object* world_floor_init(float x1, float y1, float x2, float y2) {
+	
+}
+
+struct world* world_init() {
+	
+}
+
+void world_delete(struct world *world) {
+	
+}
--- src/world.h
+++ src/world.h
@@ -1,6 +1,27 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+struct world_object {
+	int type;
+	struct world_object *next;
+};
 
+void world_object_delete(struct world_object*);
+
+struct world_floor {
+	int type;
+	struct world_object *next;
+
+	float x1, y1, x2, y2;
+};
+
+struct world_object* world_floor_init(float, float, float, float);
+
+struct world {
+	struct world_object *objects;
+};
+
+struct world* world_init();
+void world_delete(struct world*);
 
 #endif /* WORLD_H */
