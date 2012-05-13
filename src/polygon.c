ff3a2935c2f86a47de1b155ffbb512ea57e5a12d
Emily Eisenberg <xymostech@gmail.com>
2012-05-13 16:54:12 -0400

    Added basic polygon and point implementation

--- src/polygon.c
+++ src/polygon.c
@@ -1,1 +1,32 @@
 #include "polygon.h"
+
+struct point* point_init(float x, float y, float z) {
+	struct point* point = malloc(sizeof(*point));
+
+	point->x = x;
+	point->y = y;
+	point->z = z;
+
+	return point;
+}
+
+void point_delete(struct point *point) {
+	free(point);
+}
+
+struct polygon* polygon_init() {
+	struct polygon* poly = malloc(sizeof(*poly));
+
+	poly->points = NULL;
+
+	return poly;
+}
+
+void polygon_delete(struct polygon *poly) {
+	free(poly);
+}
+
+void polygon_add_point(struct polygon *poly, struct point *point) {
+	point->next = poly->points;
+	poly->points = point;
+}
--- src/polygon.h
+++ src/polygon.h
@@ -1,6 +1,26 @@
 #ifndef POLYGON_H
 #define POLYGON_H
 
+#include <stdlib.h>
 
+struct point {
+	float x, y, z;
+
+	struct point *next;
+};
+
+struct point* point_init(float, float, float);
+void point_delete(struct point*);
+
+struct polygon {
+	struct point *points;
+
+	struct polygon *next;
+};
+
+struct polygon* polygon_init();
+void polygon_delete(struct polygon*);
+
+void polygon_add_point(struct polygon*, struct point*);
 
 #endif /* POLYGON_H */
