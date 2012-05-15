c7f65e43e2ba0ca7a1e77650eb04e6e7ace29f4f
Emily Eisenberg <xymostech@gmail.com>
2012-05-15 17:34:48 -0400

    Add basic font functions

--- src/font.c
+++ src/font.c
@@ -1,1 +1,15 @@
 #include "font.h"
+
+struct font* font_load(char *filename) {
+	struct font *font = malloc(sizeof(*font));
+
+	return font;
+}
+
+void font_delete(struct font *font) {
+	free(font);
+}
+
+void font_draw(struct font *font, char *string) {
+	
+}
--- src/font.h
+++ src/font.h
@@ -1,6 +1,15 @@
 #ifndef FONT_H
 #define FONT_H
 
+#include <stdlib.h>
 
+struct font {
+	
+};
+
+struct font* font_load(char*);
+void font_delete(struct font*);
+
+void font_draw(struct font*, char *string);
 
 #endif /* FONT_H */
