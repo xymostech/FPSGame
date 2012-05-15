d9e5caf3b8db36fb9c789fd6a991239915a6438a
Emily Eisenberg <xymostech@gmail.com>
2012-05-15 13:15:35 -0400

    Add basic hud functions

--- src/hud.c
+++ src/hud.c
@@ -1,1 +1,17 @@
 #include "hud.h"
+
+struct hud* hud_init() {
+	struct hud *hud = malloc(sizeof(*hud));
+
+	return hud;
+}
+
+void hud_delete(struct hud *hud) {
+	free(hud);
+}
+
+void hud_draw(struct hud *hud) {
+	window_set_2d();
+
+	window_set_perspective();
+}
--- src/hud.h
+++ src/hud.h
@@ -1,6 +1,15 @@
 #ifndef HUD_H
 #define HUD_H
 
+#include "window.h"
 
+struct hud {
+	
+};
+
+struct hud* hud_init();
+void hud_delete(struct hud*);
+
+void hud_draw(struct hud*);
 
 #endif /* HUD_H */
