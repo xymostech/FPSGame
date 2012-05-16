9d2d320ef75d2fd41455dad29e2a8fcea375657f
Emily Eisenberg <xymostech@gmail.com>
2012-05-16 19:59:43 -0400

    Added basic vector functions

--- src/vector.c
+++ src/vector.c
@@ -1,1 +1,36 @@
 #include "vector.h"
+
+struct vector vect(float x, float y, float z) {
+	struct vector v = {x, y, z};
+	return v;
+}
+
+struct vector vector_add(struct vector a, struct vector b) {
+	return vect(a.x + b.x, a.y + b.y, a.z + b.z);
+}
+
+struct vector vector_sub(struct vector a, struct vector b) {
+	return vect(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+struct vector vector_mult(struct vector a, float scale) {
+	return vect(a.x * scale, a.y * scale, a.z * scale);
+}
+
+struct vector vector_div(struct vector a, float scale) {
+	return vect(a.x / scale, a.y / scale, a.z / scale);
+}
+
+float vector_len(struct vector a) {
+	return sqrt(a.x*a.x + a.y*a.y + a.z*a.z);
+}
+
+float vector_dot(struct vector a, struct vector b) {
+	return a.x*b.x + a.y*b.y + a.z*b.z;
+}
+
+struct vector vector_cross(struct vector a, struct vector b) {
+	return vect(a.y * b.z - a.z * b.y,
+	            a.z * b.x - a.x * b.z,
+	            a.x * b.y - a.y * b.x);
+}
--- src/vector.h
+++ src/vector.h
@@ -1,8 +1,22 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <math.h>
+
 struct vector {
 	float x, y, z;
 };
+
+struct vector vect(float, float, float);
+
+struct vector vector_add(struct vector, struct vector);
+struct vector vector_sub(struct vector, struct vector);
+struct vector vector_mult(struct vector, float);
+struct vector vector_div(struct vector, float);
+
+float vector_len(struct vector);
+
+float vector_dot(struct vector, struct vector);
+struct vector vector_cross(struct vector, struct vector);
 
 #endif /* VECTOR_H */
