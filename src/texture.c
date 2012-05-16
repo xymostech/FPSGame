d609f40ab71d922eacebb59737652a288dc7d830
Emily Eisenberg <xymostech@gmail.com>
2012-05-16 11:21:21 -0400

    Add framework for texture loading/using

--- src/texture.c
+++ src/texture.c
@@ -1,1 +1,13 @@
 #include "texture.h"
+
+struct texture* texture_load(char *filename) {
+	
+}
+
+void texture_delete(struct texture *texture) {
+	
+}
+
+void texture_use(struct texture *texture) {
+	
+}
--- src/texture.h
+++ src/texture.h
@@ -1,6 +1,15 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include <GL/glfw.h>
 
+struct texture {
+	GLuint tex;
+};
+
+struct texture* texture_load(char*);
+void texture_delete(struct texture*);
+
+void texture_use(struct texture*);
 
 #endif /* TEXTURE_H */
