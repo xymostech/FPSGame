70e8fdfa5fe4ff83d63079fce7b904917c9792d9
Emily Eisenberg <xymostech@gmail.com>
2012-05-19 09:25:06 -0400

    Add basic server creation and connection code

--- src/server.c
+++ src/server.c
@@ -1,1 +1,39 @@
 #include "server.h"
+
+struct server* server_init() {
+	struct server *server = malloc(sizeof(*server));
+
+	return server;
+}
+
+void server_delete(struct server *server) {
+	free(server);
+}
+
+void server_connect(struct server *server) {
+	struct addrinfo hints, *info, *p;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_DGRAM;
+
+	int status;
+
+	if ((status = getaddrinfo("127.0.0.1", "1234", &hints, &info)) != 0) {
+		fprintf(stderr, "Error with getaddrinfo: %s\n", gai_strerror(status));
+		return;
+	}
+
+	for (p=info; p!=NULL; p=p->ai_next) {
+		if ((server->socket = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			perror("socket");
+			continue;
+		}
+		break;
+	}
+
+	if (p == NULL) {
+		fprintf(stderr, "Error making socket\n");
+		return;
+	}
+}
--- src/server.h
+++ src/server.h
@@ -1,6 +1,24 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
+struct server {
+	int socket;
+};
+
+struct server* server_init();
+void server_delete(struct server*);
+
+void server_connect(struct server*);
 
 #endif /* SERVER_H */
