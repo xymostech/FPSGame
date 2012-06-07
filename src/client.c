134935db8efe3d1223ea6fecd7d3b3a33b76538b
Emily Eisenberg <xymostech@gmail.com>
2012-06-07 01:18:56 -0700

    Comment server client functions

--- src/client.c
+++ src/client.c
@@ -1,24 +1,49 @@
 #include "client.h"
 
+/*
+ * client_init(id, addr, addrlen)
+ *
+ * basic setup of a client
+ */
 struct client* client_init(int id, struct sockaddr *addr, size_t addrlen) {
 	struct client *client = malloc(sizeof(*client));
 
 	client->id = id;
+
+	/* memcpy the address to store it */
 	memcpy(&client->addr, addr, addrlen);
 	client->addrlen = addrlen;
 
 	return client;
 }
 
+/*
+ * client_delete(client)
+ *
+ * basic delete of a client
+ */
 void client_delete(struct client *client) {
 	free(client);
 }
 
+/*
+ * client_sendpacket(client, sock, packet, size)
+ *
+ * sends a packet of a given size to a client
+ */
 int client_sendpacket(struct client *client, int sock, unsigned char *packet, int size) {
 	return sendto(sock, packet, size, 0, (struct sockaddr*)&client->addr, client->addrlen);
 }
 
+/*
+ * client_lookup(clients, id)
+ *
+ * looks up a client by its id in a linked list
+ *
+ * returns: the client with id==id, or NULL if none found
+ */
 struct client* client_lookup(struct client *clients, int id) {
+	/* use a loop because clients are doubly-linked */
 	struct client *loop = clients->next;
 	while (loop != clients) {
 		if (loop->id == id) {
--- src/client.h
+++ src/client.h
@@ -1,3 +1,8 @@
+/*
+ * client.h
+ *
+ * server-side storage of connected players' data
+ */
 #ifndef CLIENT_H
 #define CLIENT_H
 
@@ -12,21 +17,28 @@
 #include "servplayer.h"
 
 struct client {
+	/* global client id */
 	int id;
 
+	/* storage of their address, to send data to */
 	struct sockaddr_storage addr;
 	size_t addrlen;
 
+	/* storage of their player properties */
 	struct servplayer player;
 
+	/* linked list pointers */
 	struct client *next, *prev;
 };
 
+/* basic init/delete functions */
 struct client* client_init(int, struct sockaddr*, size_t);
 void client_delete(struct client*);
 
+/* send a packet to a client */
 int client_sendpacket(struct client*, int, unsigned char*, int);
 
+/* lookup a client by its id in a linked list */
 struct client* client_lookup(struct client*, int);
 
 #endif /* CLIENT_H */
