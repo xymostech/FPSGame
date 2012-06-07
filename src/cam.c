9adabaeea139e0af5fdc1dd737a1673d0675682f
Emily Eisenberg <xymostech@gmail.com>
2012-06-07 01:01:54 -0700

    Comment cam functions

--- src/cam.c
+++ src/cam.c
@@ -1,7 +1,13 @@
 #include "cam.h"
 
+/*
+ * setup_cam(struct cam)
+ *
+ * takes a camera struct, and correctly sets up the view perspective
+ */
 void setup_cam(struct cam *my_cam) {
 	glLoadIdentity();
+	/* TODO: don't use gluLookAt, roll my own glRotates or glMultMatrix */
 	gluLookAt(my_cam->x, my_cam->y, my_cam->z,
 	          my_cam->x+cos(my_cam->xang)*sin(my_cam->yang),
 	          my_cam->y+cos(my_cam->yang),
--- src/cam.h
+++ src/cam.h
@@ -1,14 +1,24 @@
+/*
+ * cam.h
+ *
+ * functions for setting a virtual camera by setting the perspective
+ */
+
 #ifndef CAM_H
 #define CAM_H
 
 #include <GL/glfw.h>
 #include <math.h>
 
+/* cam structure */
 struct cam {
+	/* cam position */
 	float x, y, z;
+	/* cam angles */
 	float xang, yang;
 };
 
+/* setup the camera */
 void setup_cam(struct cam*);
 
 #endif /* CAM_H */
