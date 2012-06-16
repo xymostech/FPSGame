61968035b08733518fc94728e98ab9be41c05a40
Emily Eisenberg <xymostech@gmail.com>
2012-06-16 13:36:10 -0700

    Comment hud.h/hud.c

--- src/hud.c
+++ src/hud.c
@@ -1,25 +1,31 @@
 #include "hud.h"
 
+/* initialize the hud */
 struct hud* hud_init() {
 	struct hud *hud = malloc(sizeof(*hud));
 
+	/* load a pre-defined font */
 	hud->my_font = font_load("res/LiberationMono-Regular.ttf");
 
 	return hud;
 }
 
+/* free up the hud and font */
 void hud_delete(struct hud *hud) {
 	font_delete(hud->my_font);
 	free(hud);
 }
 
+/* draw the hud */
 void hud_draw(struct hud *hud) {
+	/* switch to 2D coordinates */
 	window_set_2d();
 	
 	glLoadIdentity();
 
 	glColor3f(0, 0, 1);
 
+	/* draw a diamond crosshair */
 	glBegin(GL_LINE_LOOP);
 		glVertex2f(0, 5);
 		glVertex2f(5, 0);
@@ -27,5 +33,6 @@ void hud_draw(struct hud *hud) {
 		glVertex2f(-5, 0);
 	glEnd();
 
+	/* switch back to perspective */
 	window_set_perspective();
 }
--- src/hud.h
+++ src/hud.h
@@ -1,16 +1,35 @@
+/*
+ * hud.h
+ *
+ * Code to draw a HUD (heads up display) by
+ * changing the display to draw in 2D, and then
+ * drawing flat objects, before changing the
+ * perspective back
+ *
+ * Currently not a lot is implemented, and everything
+ * has to be hard-coded, but there are plans in the
+ * future for much more open-ended HUD elements
+ */
 #ifndef HUD_H
 #define HUD_H
 
 #include "window.h"
 #include "font.h"
 
+/*
+ * hud struct
+ *
+ * right now only contains a font
+ */
 struct hud {
 	struct font *my_font;
 };
 
+/* create and destroy a hud */
 struct hud* hud_init();
 void hud_delete(struct hud*);
 
+/* draw the hud to the screen */
 void hud_draw(struct hud*);
 
 #endif /* HUD_H */
