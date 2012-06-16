b2ee316276fe10ea1ea558e01301258b48f4059c
Emily Eisenberg <xymostech@gmail.com>
2012-06-16 14:39:24 -0700

    Comment model.h/model.c

--- src/model.c
+++ src/model.c
@@ -1,18 +1,33 @@
 #include "model.h"
 
+/*
+ * parse_vertex
+ *
+ * parses out a vertex from an OBJ string:
+ * takes a string of the form "V/T/N" and stores the
+ * values of V, T, and N. If T or N doesn't exist,
+ * 0 is stored in the value
+ */
 void parse_vertex(char *str, int *vert, int *tvert, int *nvert) {
+	/* save iterates through the string,
+	 * token stores the beginning of the last
+	 * token character */
 	char *token, *save;
 	token = save = str;
+	/* move save up to the next character */
 	while (*save != '/' && *save) {
 		++save;
 	}
+	/* if we hit the end, save V and set
+	 * T and N to 0 */
 	if (!*save) {
 		*vert = atoi(token);
 		*tvert = 0;
 		*nvert = 0;
 		return;
 	}
-	*save = 0;
+
+	/* continue for the second and third tokens */
 	*vert = atoi(token);
 	token = save+1;
 	while (*save != '/' && *save) {
@@ -31,6 +46,7 @@ void parse_vertex(char *str, int *vert, int *tvert, int *nvert) {
 	*nvert = atoi(token);
 }
 
+/* lookup a point by indes in the linked list of points */
 struct point* lookup_point(struct point *points, int num_points, int point) {
 	if (point == 0) {
 		return NULL;
@@ -42,9 +58,11 @@ struct point* lookup_point(struct point *points, int num_points, int point) {
 	return points;
 }
 
+/* load a model by filename */
 struct model* model_load(char *filename) {
 	struct model *model = malloc(sizeof(*model));
 
+	/* set the polygon and textures to null */
 	model->polygons = NULL;
 	model->texture = NULL;
 
@@ -61,16 +79,34 @@ struct model* model_load(char *filename) {
 	struct point *norm_points = NULL;
 	int num_norm_points = 0;
 
+	/* the point to be used when
+	 * a point is not found or given */
+	struct point zero_point;
+	zero_point.x = 0;
+	zero_point.y = 0;
+	zero_point.z = 0;
+	zero_point.tu = 0;
+	zero_point.tv = 0;
+	zero_point.nx = 0;
+	zero_point.ny = 0;
+	zero_point.nz = 0;
+
+	/* read in lines from the file, and parse them */
+	/* line = malloc(0) allows getline to be used, without
+	 * taking up any actual space */
 	char *line = malloc(0);
 	size_t size = 0;
 	getline(&line, &size, file);
 	while (!feof(file)) {
 		char *token, *save, *rest;
+		/* split the line by whitespace */
 		token = strtok_r(line, " \t\n", &save);
+		/* ignore empty lines */
 		if (token == NULL) {
 			continue;
 		}
 		if (strcmp(token, "v") == 0) {
+			/* load a vertex */
 			rest = strtok_r(NULL, "\n", &save);
 			float x, y, z;
 			sscanf(rest, "%f %f %f", &x, &y, &z);
@@ -79,6 +115,7 @@ struct model* model_load(char *filename) {
 			points = point;
 			num_points++;
 		} else if (strcmp(token, "vt") == 0) {
+			/* load a vertex texture */
 			rest = strtok_r(NULL, "\n", &save);
 			float x, y;
 			sscanf(rest, "%f %f", &x, &y);
@@ -87,6 +124,7 @@ struct model* model_load(char *filename) {
 			tex_points = point;
 			num_tex_points++;
 		} else if (strcmp(token, "vn") == 0) {
+			/* load a vertex normal */
 			rest = strtok_r(NULL, "\n", &save);
 			float x, y, z;
 			sscanf(rest, "%f %f %f", &x, &y, &z);
@@ -95,31 +133,32 @@ struct model* model_load(char *filename) {
 			norm_points = point;
 			num_norm_points++;
 		} else if (strcmp(token, "f") == 0) {
+			/* load a face */
+			/* create a polygon */
 			struct polygon *polygon = polygon_init();
+
+			/* get all the points in the polygon */
 			token = strtok_r(NULL, " \t\n", &save);
 			int point_count = 0;
 			while (token) {
 				int v, tv, nv;
+				/* parse out the vertex */
 				parse_vertex(token, &v, &tv, &nv);
 				point_count++;
-				struct point zero_point;
-				zero_point.x = 0;
-				zero_point.y = 0;
-				zero_point.z = 0;
-				zero_point.tu = 0;
-				zero_point.tv = 0;
-				zero_point.nx = 0;
-				zero_point.ny = 0;
-				zero_point.nz = 0;
+				/* lookup the point, texture, and normal */
 				struct point *ppoint = lookup_point(points, num_points, v);
 				struct point *tpoint = lookup_point(tex_points, num_tex_points, tv);
+				struct point *npoint = lookup_point(norm_points, num_norm_points, nv);
+				/* if the texture or normal aren't given,
+				 * use the zero point instead */
 				if (!tpoint) {
 					tpoint = &zero_point;
 				}
-				struct point *npoint = lookup_point(norm_points, num_norm_points, nv);
 				if (!npoint) {
 					npoint = &zero_point;
 				}
+
+				/* create the point */
 				struct point *point =
 				      point_init(ppoint->x, ppoint->y, ppoint->z,
 				                 tpoint->tu, tpoint->tv,
@@ -127,12 +166,16 @@ struct model* model_load(char *filename) {
 				polygon_add_point(polygon, point);
 				token = strtok_r(NULL, " \t\n", &save);
 			}
+
+			/* if there are three points, then we can do
+			 * triangular hittesting */
 			if (point_count == 3) {
 				polygon_setup_hittest(polygon);
 			}
 			polygon->next = model->polygons;
 			model->polygons = polygon;
 		} else if (strcmp(token, "usetex") == 0) {
+			/* use a given texture */
 			rest = strtok_r(NULL, "\n", &save);
 			model->texture = texture_load(rest);
 		}
@@ -140,6 +183,7 @@ struct model* model_load(char *filename) {
 	}
 	free(line);
 
+	/* free the loaded points, because we made copies earlier */
 	struct point *next;
 	while (points) {
 		next = points->next;
@@ -162,6 +206,7 @@ struct model* model_load(char *filename) {
 	return model;
 }
 
+/* free the polygons, texture, and model */
 void model_delete(struct model *model) {
 	struct polygon *poly = model->polygons, *next_poly;
 	while (poly) {
@@ -173,11 +218,14 @@ void model_delete(struct model *model) {
 	free(model);
 }
 
+/* draw the model */
 void model_draw(struct model *model) {
+	/* use the given texture */
 	if (model->texture) {
 		glEnable(GL_TEXTURE_2D);
 		texture_use(model->texture);
 	}
+	/* go through the polygon list, and draw each of them */
 	struct polygon *poly = model->polygons;
 	while (poly) {
 		polygon_draw(poly);
@@ -188,11 +236,20 @@ void model_draw(struct model *model) {
 	}
 }
 
+/*
+ * model_hittest
+ *
+ * Test whether a ray hits the model by calling
+ * hittest on all of the polygons within the model
+ */
 float model_hittest(struct model *model, struct vector start, struct vector dir) {
+	/* cycle through each polygon */
 	struct polygon *poly = model->polygons;
 	float best = -1, test;
 	while (poly) {
+		/* check if the ray hits this polygon */
 		test = polygon_hittest(poly, start, dir);
+		/* if it does, and is closer, store that */
 		if (best < 0) {
 			best = test;
 		} else if (test > 0 && test < best) {
@@ -200,5 +257,6 @@ float model_hittest(struct model *model, struct vector start, struct vector dir)
 		}
 		poly = poly->next;
 	}
+	/* store the best polygon hit, which will be -1 if none were hit */
 	return best;
 }
--- src/model.h
+++ src/model.h
@@ -1,3 +1,9 @@
+/*
+ * model.h
+ *
+ * Functions for loading, drawing, and
+ * testing hittest for models
+ */
 #ifndef MODEL_H
 #define MODEL_H
 
@@ -10,18 +16,26 @@
 #include "vector.h"
 #include "texture.h"
 
+/* model struct */
 struct model {
+	/* filename that the model was loaded from */
 	char *file;
 
+	/* texture used for the model */
 	struct texture *texture;
 
+	/* linked list of polygons */
 	struct polygon *polygons;
 };
 
+/* load and delete models */
 struct model* model_load(char*);
 void model_delete(struct model*);
 
+/* draw a model */
 void model_draw(struct model*);
+
+/* hittest a model */
 float model_hittest(struct model*, struct vector, struct vector);
 
 #endif /* MODEL_H */
