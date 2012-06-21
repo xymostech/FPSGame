6078b4d686df6550a25d2f56b3f638bf71e61649
Emily Eisenberg <xymostech@gmail.com>
2012-06-20 22:00:04 -0700

    Comment mouse.h/mouse.c

--- src/mouse.c
+++ src/mouse.c
@@ -1,27 +1,34 @@
 #include "mouse.h"
 
+/* check the state of the left mouse button */
 int mouse_left_pressed() {
 	return glfwGetMouseButton(GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
 }
 
+/* check the state of the right mouse button */
 int mouse_right_pressed() {
 	return glfwGetMouseButton(GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
 }
 
+/* check the mouse x position */
 int mouse_window_x() {
 	int x, y;
 	glfwGetMousePos(&x, &y);
 	return x - *window_width() / 2;
 }
 
+/* check the mouse y position */
 int mouse_window_y() {
 	int x, y;
 	glfwGetMousePos(&x, &y);
 	return *window_height() / 2 - y;
 }
 
+/* check how much the mouse has changed since the last call */
 void mouse_get_mouselook_diff(int *x, int *y) {
+	/* set the mouse position */
 	*x = mouse_window_x();
 	*y = mouse_window_y();
+	/* set the mouse position to the middle */
 	glfwSetMousePos(*window_width()/2, *window_height()/2);
 }
--- src/mouse.h
+++ src/mouse.h
@@ -1,3 +1,8 @@
+/*
+ * mouse.h
+ *
+ * mouse functions to check the state of the mouse
+ */
 #ifndef MOUSE_H
 #define MOUSE_H
 
@@ -5,12 +10,15 @@
 
 #include "window.h"
 
+/* check if mouse buttons are pressed */
 int mouse_left_pressed();
 int mouse_right_pressed();
 
+/* check the mouse position */
 int mouse_window_x();
 int mouse_window_y();
 
+/* find how much the mouse has moved since the last check */
 void mouse_get_mouselook_diff(int *x, int *y);
 
 #endif /* MOUSE_H */
